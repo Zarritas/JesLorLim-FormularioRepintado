d4f461d65b237fe0c4528e3192d790fb69cd53bf
Jesus Lorenzo <jesus@iesclaradelrey.es>
2023-11-20 12:24:23 +0100

    Primer commit y creación del programa

---
+++ src/main/java/jlorenzolimon/org/formulariorepintado/FormularioRepintadoApplication.java
@@ -0,0 +1,13 @@
+package jlorenzolimon.org.formulariorepintado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormularioRepintadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormularioRepintadoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jlorenzolimon/org/formulariorepintado/FormularioRepintadoApplicationTests.java
@@ -0,0 +1,13 @@
+package jlorenzolimon.org.formulariorepintado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormularioRepintadoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
